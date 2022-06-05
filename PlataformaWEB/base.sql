4befc5fe71de6eaf42338d414618bc651dbc6afe
Hugo Adi Jimenez Martinez <hugoadijm@gmail.com>
2022-06-05 03:12:00 -0500

    Add files via upload
    Nueva base

--- PlataformaWEB/base.sql
+++ PlataformaWEB/base.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-06-2022 a las 08:17:27
+-- Tiempo de generación: 05-06-2022 a las 10:11:16
 -- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.5
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,55 +18,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `actividad`
---
-
-CREATE TABLE `actividad` (
-  `Id` int(11) NOT NULL,
-  `Nombre` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `administrador`
---
-
-CREATE TABLE `administrador` (
-  `Id` int(10) NOT NULL,
-  `No.empleado` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `administrador`
---
-
-INSERT INTO `administrador` (`Id`, `No.empleado`) VALUES
-(1, 238420);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `agenda`
---
-
-CREATE TABLE `agenda` (
-  `Id` int(11) NOT NULL,
-  `hora` time NOT NULL,
-  `fecha` date NOT NULL,
-  `alumno` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `alumno`
 --
 
 CREATE TABLE `alumno` (
   `Id` int(10) NOT NULL,
   `Progreso` decimal(10,0) NOT NULL,
-  `Tutor` int(11) NOT NULL,
+  `Tutor` varchar(11) NOT NULL,
   `Boleta` int(11) NOT NULL,
   `Id_Grupo` int(11) NOT NULL,
   `Id_Bloque` int(11) NOT NULL,
@@ -79,117 +37,11 @@ CREATE TABLE `alumno` (
 --
 
 INSERT INTO `alumno` (`Id`, `Progreso`, `Tutor`, `Boleta`, `Id_Grupo`, `Id_Bloque`, `Id_Actividad`, `Id_Material`) VALUES
-(3, '1', 1, 2022010101, 1, 1, 1, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `bloque`
---
-
-CREATE TABLE `bloque` (
-  `Id` int(11) NOT NULL,
-  `Tema` text NOT NULL,
-  `Nombre` text NOT NULL,
-  `Id_Actividad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `grupo`
---
-
-CREATE TABLE `grupo` (
-  `Id` int(11) NOT NULL,
-  `Id_bloque` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `material`
---
-
-CREATE TABLE `material` (
-  `Id` int(11) NOT NULL,
-  `Nombre` text NOT NULL,
-  `Tema` text NOT NULL,
-  `Tipo` text NOT NULL,
-  `Estatus` text NOT NULL,
-  `Atendida` text DEFAULT NULL,
-  `Observacion` text DEFAULT NULL,
-  `Archivo` mediumblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `profesor`
---
-
-CREATE TABLE `profesor` (
-  `Id` int(10) NOT NULL,
-  `No.empleado` int(10) NOT NULL,
-  `Id_grupo` int(11) NOT NULL,
-  `Id_agenda` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `profesor`
---
-
-INSERT INTO `profesor` (`Id`, `No.empleado`, `Id_grupo`, `Id_agenda`) VALUES
-(2, 2831123, 1, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE `usuario` (
-  `Id` int(10) NOT NULL,
-  `Nombre` varchar(50) NOT NULL,
-  `Nombre_usuario` varchar(50) NOT NULL,
-  `Contraseña` varchar(20) NOT NULL,
-  `Tipo` varchar(20) NOT NULL,
-  `Telefono` int(11) NOT NULL,
-  `CURP` varchar(18) NOT NULL,
-  `Correo_electronico` varchar(50) NOT NULL,
-  `Correo_electronico_alterno` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`Id`, `Nombre`, `Nombre_usuario`, `Contraseña`, `Tipo`, `Telefono`, `CURP`, `Correo_electronico`, `Correo_electronico_alterno`) VALUES
-(1, 'Prueba', 'Prueba', 'prueba', 'Administrador', 553119461, 'CGH120486JEPGMSLI8', 'prueba@gmail.com', 'prueba2@gmail.com'),
-(2, 'Prueba', 'Prueba', 'prueba', 'Profesor', 555555555, 'CUSM192805IFLG09PW', 'prueba@gmail.com', 'prueba2@gmail.com'),
-(3, 'Prueba', 'Prueba', 'prueba', 'Alumno', 553819471, 'PFYM1029IMG0971PQH', 'prueba@gmail.com', 'prueba2@gmail.com');
+(7, '0', '0', 201853251, 3, 0, 0, 0);
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `actividad`
---
-ALTER TABLE `actividad`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indices de la tabla `administrador`
---
-ALTER TABLE `administrador`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indices de la tabla `agenda`
---
-ALTER TABLE `agenda`
-  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indices de la tabla `alumno`
@@ -198,64 +50,12 @@ ALTER TABLE `alumno`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indices de la tabla `bloque`
---
-ALTER TABLE `bloque`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indices de la tabla `grupo`
---
-ALTER TABLE `grupo`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indices de la tabla `material`
---
-ALTER TABLE `material`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indices de la tabla `profesor`
---
-ALTER TABLE `profesor`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`Id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- Restricciones para tablas volcadas
 --
-
---
--- Filtros para la tabla `administrador`
---
-ALTER TABLE `administrador`
-  ADD CONSTRAINT `administrador_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `usuario` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `alumno`
 --
 ALTER TABLE `alumno`
   ADD CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `usuario` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `profesor`
---
-ALTER TABLE `profesor`
-  ADD CONSTRAINT `profesor_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `usuario` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
