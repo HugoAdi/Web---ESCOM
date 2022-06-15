3868089251e6500de09f10125c5dff27525257cb
JesusID80 <66971023+JesusID80@users.noreply.github.com>
2022-06-14 23:34:16 -0500

    Update Soporte.php

--- PlataformaWEB/Alumno/Soporte.php
+++ PlataformaWEB/Alumno/Soporte.php
@@ -76,6 +76,12 @@
         </div>
         <div id="contenido">
             <div id="articulo">
+		    <?php
+                            error_reporting(0);
+                            include("conexion.php");
+                            $id=$_POST['id'];
+                            $id1=mysqli_query($conexion, "SELECT * FROM grupo WHERE Profesor='6'");
+                        ?>
                 <fieldset>
                     <legend><h3>Soporte</h3></legend>
                     Duda: <input type="text" required placeholder="Palabra secreta">
