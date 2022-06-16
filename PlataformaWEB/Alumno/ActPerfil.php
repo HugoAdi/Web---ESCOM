5ca2117a7990bf668336336ffc2e874097932b69
JesusID80 <66971023+JesusID80@users.noreply.github.com>
2022-06-16 15:06:11 -0500

    Update ActPerfil.php

--- PlataformaWEB/Alumno/ActPerfil.php
+++ PlataformaWEB/Alumno/ActPerfil.php
@@ -4,7 +4,7 @@
         <title>Actualizacion de perfil</title>
         <meta charset="utf-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="./recursos/aluindex2.css">
+        <link rel="stylesheet" type="text/css" href="./recursos/aluindex.css">
         <link rel="stylesheet" type="text/css" href="./recursos/navAlumno.css">
         <link rel="stylesheet" type="text/css" href="./recursos/aluheader.css">
 	    <link rel="stylesheet" type="text/css" href="../reloj.css">
